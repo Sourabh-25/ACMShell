a63b2166d0a86dd80b3afecd3c1cc18005dd7302
drcode03 <ijatingrover@gmail.com>
2023-05-28 18:00:43 +0530

    comments added

--- shell.c
+++ shell.c
@@ -14,13 +14,8 @@ int ACMShell_help(char **args);
 int ACMShell_exit(char **args);
 int sh_bg(char **args);
 int ACMShell_history(char **args);
-//a struct for history's linked list:
-struct Node {
-    char *str;
-    struct Node* next;
-};
-struct Node* head = NULL;
-struct Node* cur = NULL;
+
+
 // builtins
 char *builtin_str[] = {
     "cd",
@@ -73,8 +68,64 @@ int ACMShell_help(char **args)
 
   return 1;
 }
-//creating functions and a struct for history:
+//a struct for history's linked list:
+struct Node {
+    char *str;
+    struct Node* next;
+};
+struct Node* head = NULL;
+struct Node* cur = NULL;
+//implementing utility functions for history:
+char* strAppend(char* str1, char* str2)
+{
+	char* str3 = (char*)malloc(sizeof(char*)*(strlen(str1)+strlen(str2)));
+  strcpy(str3, str1);
+  strcat(str3, str2);
+	return str3;
+}
+void add_to_hist(char **args){
+  if(head==NULL){
+    head = (struct Node *)malloc(sizeof(struct Node));
+      head->str = (char *)malloc(0x1000);
+  char *str1 = " ";
+  if (args[1] == NULL) 
+     strcpy(head->str,strAppend(args[0],str1));
+  else
+{  
+  strcpy(head->str,strAppend(args[0],str1));
+  strcpy(head->str, strAppend(head->str, args[1]));
+  }
+  head->next = NULL;
+  cur = head;
+  }
+else{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    cur->next = ptr;
+    ptr->str = (char *)malloc(0x1000);
+      char *str1 = " ";
+  if (args[1] == NULL) 
+     strcpy(ptr->str,strAppend(args[0],str1));
+  else
+{  
+  strcpy(ptr->str,strAppend(args[0],str1));
+  strcpy(ptr->str, strAppend(ptr->str, args[1]));
+  }
+    ptr->next = NULL;
+    cur = ptr;
+}
+}
+//creating a function to display history:
 
+int ACMShell_history(char **args){  
+   struct Node* ptr = head;
+    int i = 1;
+    while (ptr != NULL)
+    {
+    printf(" %d %s\n",i++,ptr->str);
+    ptr = ptr->next;
+    }
+  return 1; 
+  }
 
 
 // exit ACMShell
@@ -106,16 +157,7 @@ int sh_bg(char **args)
   return 1;
 }
 
-int ACMShell_history(char **args){  
-   struct Node* ptr = head;
-    int i = 1;
-    while (ptr != NULL)
-    {
-    printf(" %d %s\n",i++,ptr->str);
-    ptr = ptr->next;
-    }
-  return 1; 
-  }
+
 // some processes such as all the builtins just change the properties of
 // spawned child and not of the parent process.For them we have builtins and for
 // rest, we have following:
@@ -150,56 +192,7 @@ int ACMShell_launch(char **args)
 
   return 1;
 }
-char* strAppend(char* str1, char* str2)
-{
-	char* str3 = (char*)malloc(sizeof(char*)*(strlen(str1)+strlen(str2)));
-  // char *str = " ";
-  strcpy(str3, str1);
-  // strcpy(str3, str);
-  strcat(str3, str2);
-	//printf("%s appended to %s --> %s\n",str1,str2,str3);
-	return str3;
-}
-void add_to_hist(char **args){
-  if(head==NULL){
-    head = (struct Node *)malloc(sizeof(struct Node));
-      head->str = (char *)malloc(0x1000);
-      // if(NULL!=args[1])
-      //   strcpy(head->str,args[0]);
-      //   else
-  char *str1 = " ";
-  if (args[1] == NULL) 
-  {      strcpy(head->str,strAppend(args[0],str1));}
-  else
-{  
-  strcpy(head->str,strAppend(args[0],str1));
-  strcpy(head->str, strAppend(head->str, args[1]));
-  }
-  //   strcpy(head->str,args[0]);
-  // if(NULL!=args[1])
-  //   strcpy(head->str,args[1]);
 
-  head->next = NULL;
-  cur = head;
-  }
-else{
-    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
-    cur->next = ptr;
-    ptr->str = (char *)malloc(0x1000);
-      char *str1 = " ";
-  if (args[1] == NULL) 
-  {     
-     strcpy(ptr->str,strAppend(args[0],str1));
-  }
-  else
-{  
-  strcpy(ptr->str,strAppend(args[0],str1));
-  strcpy(ptr->str, strAppend(ptr->str, args[1]));
-  }
-    ptr->next = NULL;
-    cur = ptr;
-}
-}
 // executing the builtins and calling launc() for rest.
 int ACMShell_execute(char **args)
 {
